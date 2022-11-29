b245017daa8ef3e236f4c2122a59e631fed0e1cf
w-dze <wdu05888@gmail.com>
2022-11-29 14:01:17 -0600

    basic

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -1,4 +1,34 @@
 
 public class RingBuffer {
-
+	public RingBuffer(int capacity) {
+		
+	}
+	
+	public int size() {
+		return 0;
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public boolean isFull() {
+		return false;
+	}
+	
+	public void enqueue(double x) {
+		
+	}
+	
+	public double dequeue() {
+		return 0.0;
+	}
+	
+	public double peek() {
+		return 0.0;
+	}
+	
+	public String toString() {
+		return null;
+	}
 }
