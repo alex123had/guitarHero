b87b5632f6399cb5ebe191528e9f2c1043e2e7b3
w-dze <wdu05888@gmail.com>
2022-12-01 14:00:16 -0600

    fixing toString()

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -1,78 +1,112 @@
 import java.util.NoSuchElementException;
 
 public class RingBuffer {
-	double[] buffer;
-	int size = 0;
-	int first;
-	int last;
-	
-	public RingBuffer(int capacity) {
-		buffer = new double[capacity];
-		first = 0; 
-		last = 0;
-	}
-	
-	public int size() {
-		return size;
-	}
-	
-	public boolean isEmpty() {
-		return size == 0; 
-	}
-	
-	public boolean isFull() {
-		return size == buffer.length;
-	}
-	
-	public void enqueue(double x) {
-		
-		if (isFull()) {
-			throw new IllegalStateException();
-		}
-		
-		buffer[last]= x;
-		last++;
-		
-		if (last >= buffer.length-1) {
-			last = 0;
-		}
-		
-		size++;		
-	}
-	
-	public double dequeue() {
-		if (isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		
-		double temp = buffer[first];
-		first++;
-		
-		size--;
-		
-		if (first>= buffer.length) {
-			first = 0;
-		}
-		
-		return temp;
-	}
-	
-	public double peek() {
-		if (isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		return buffer[first];
-	}
-	
-	public String toString() {
-		String temp = "[";
-		
-		for (int i = first; i<last-1; i++) {
-			temp = temp + buffer[i] + ", ";
-		}
-		temp += buffer[last-1] + "]";
-	
-		return temp;
-	}
-}
+  double[] buffer;
+  int size = 0;
+  int first;
+  int last;
 
+  public RingBuffer(int capacity) {
+    buffer = new double[capacity];
+    first = 0;
+    last = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public boolean isFull() {
+    return size == buffer.length;
+  }
+
+  public void enqueue(double x) {
+
+    if (isFull()) {
+      throw new IllegalStateException();
+    }
+
+    if (isEmpty()) {
+      first = 0;
+      last = 0;
+    }
+
+    buffer[last] = x;
+    last++;
+
+    if (last >= buffer.length - 1) {
+      last = 0;
+    }
+
+    size++;
+  }
+
+  public double dequeue() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    double tArray[] = new double[buffer.length];
+
+    double temp = buffer[first];
+    buffer[first] = tArray[first];
+    first++;
+
+    size--;
+
+    if (first >= buffer.length) {
+      first = 0;
+    }
+
+    if (size == 0) {
+      first = 0;
+      last = 0;
+    }
+    return temp;
+  }
+
+  public double peek() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    return buffer[first];
+  }
+
+  public String toString() {
+    double inOrder[] = new double[size()];
+    int index=0;
+
+    if (last <= first && !isEmpty()) {
+      for (int i = first; i < buffer.length; i++){
+        inOrder[index] = buffer[i];
+        index++;
+      }
+
+      for (int j = 0; j < last; j++) {
+        inOrder[index] = buffer[j];
+        index++;
+      }
+      
+    } else {
+      index = 0;
+      for (int x = 0; x < last; x++) {
+        inOrder[index] = buffer[x];
+        index++;
+      }
+    }
+
+    String temp = "[";
+
+    if (isEmpty()) {
+      return "[]";
+    }
+
+    for (int y = 0; y < inOrder.length; y++) {
+      temp += inOrder[y] + ",";
+    }
+    return temp;
+  }
+}
