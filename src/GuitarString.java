b62aafc8ee47c849c8ac7a81d7540f07ccb961aa
alex123had <archalex29@gmail.com>
2022-12-02 14:15:06 -0600

    new file
    rararara

---
+++ src/GuitarString.java
@@ -0,0 +1,23 @@
+
+public class GuitarString {
+	
+	public GuitarString(double frequency) {
+		
+	}
+	public GuitarString(double[] init) {
+		
+	}
+	public void pluck() {
+		
+	}
+	public void tic() {
+		
+	}
+	public double sample() {
+		return 0.0;
+	}
+	public int time() {
+		return 0;
+	}
+	
+}
