7e762f6709287751e80d36721515a22d412d0626
w-dze <wdu05888@gmail.com>
2022-12-06 14:39:48 -0600

    Update Guitar Hero

--- src/GuitarHero.java
+++ src/GuitarHero.java
@@ -1,4 +1,34 @@
-
 public class GuitarHero {
-
+	public static void main(String[] args) {
+	String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+	GuitarString[] notes = new GuitarString [37];
+	
+	for (int i = 0; i<notes.length; i++) {
+		int frequency = (int)(440*Math.pow(1.05960, i-25));
+		notes[i] = new GuitarString(frequency);
+	}
+	
+	while (true) {
+		if(StdDraw.hasNextKeyTyped()) {
+			char key = StdDraw.nextKeyTyped();
+			
+			int index = keyboard.indexOf(key);
+			
+			if (index!=-1) {
+				notes[index].pluck();
+			}
+			
+			double sample;
+			
+			for(int i = 0; i; i++) {
+				sample+= key[i].sample();
+			}
+			
+			StdAudio.play(sample);
+			
+			
+		}
+	}
+	
+	}
 }
